f18f71adec3ab51f93851ef7329245d1605910ac
Pekotaker <loliisdabezt@gmail.com>
2024-09-09 18:49:08 +0700

    fixes bug

--- src/api/weatherService/index.js
+++ src/api/weatherService/index.js
@@ -28,29 +28,29 @@ class WeatherService {
     }
   }
 
-  // async getCurrentLocationWeather() {
-  //   try {
-  //     const PublicIP = await CurrentPublicIPService.getCurrentPublicIP();
-  //     const params = {
-  //       city: PublicIP,
-  //     };
-  //     const response = await http.get(`/getWeatherHistory`, { params });
-  //     return response.data;
-  //   } catch (error) {
-  //     console.error("Error while fetching current location weather", error);
-  //     return null;
-  //   }
-  // }
-
   async getCurrentLocationWeather() {
     try {
-      const response = await http.get(`/getCurrentLocationWeather`);
+      const PublicIP = await CurrentPublicIPService.getCurrentPublicIP();
+      const params = {
+        city: PublicIP,
+      };
+      const response = await http.get(`/getWeatherHistory`, { params });
       return response.data;
     } catch (error) {
       console.error("Error while fetching current location weather", error);
       return null;
     }
   }
+
+  // async getCurrentLocationWeather() {
+  //   try {
+  //     const response = await http.get(`/getCurrentLocationWeather`);
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error("Error while fetching current location weather", error);
+  //     return null;
+  //   }
+  // }
 }
 
 export default new WeatherService();
